df2d24817a88184764e9b07db9b6f5171d9de249
timmymejabitoast <timmy.mejabi@toasttab.com>
2025-07-21 17:12:06 +0100

    added dashbaord components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import DashboardHeader from "@/components/DashboardHeader";
 import BalanceCards from "@/components/BalanceCards";
 import FinancialInsights from "@/components/FinancialInsights";
 import ExpenseRecords from "@/components/ExpenseRecords";
-import TransactionHistory from "@/components/TransactionHistory";
 
 export default async function DashboardPage() {
     return (
@@ -14,20 +13,10 @@ export default async function DashboardPage() {
                 <DashboardHeader />
                 
                 <main className="flex-1 p-6">
-                    <div className="mb-8">
-                        <h2 className="text-2xl font-bold mb-2">Welcome Back, Yasfin 👋</h2>
-                        <p className="text-gray-600">Here&apos;s manage your financial insights.</p>
-                    </div>
-
                     <BalanceCards />
-
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
                         <FinancialInsights />
                         <ExpenseRecords />
-                    </div>
-
-                    <div className="mt-6">
-                        <TransactionHistory />
                     </div>
                 </main>
             </div>
